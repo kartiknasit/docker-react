042ec072bd12fa6969cde12e6b70b994aae4d302
Kartik Patel <kpatel@ibaset.com>
2020-10-03 17:55:27 +0530

    Commit in feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi from docker-compose
+          Hi from docker-compose in feature branch
         </p>
         <a
           className="App-link"
